b76cdee3cc72b94140bb48405c4bbf94b2542a79
Mario_junior23 <mariojunior3251@gmail.com>
2020-11-16 17:46:43 -0300

    Atualizando produto

--- src/main/java/vendas/Controller/ProdutoController.java
+++ src/main/java/vendas/Controller/ProdutoController.java
@@ -1,11 +1,13 @@
 package vendas.Controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import vendas.Entity.Produto;
 import vendas.Repository.Produtos;
@@ -25,5 +27,19 @@ public class ProdutoController {
  	public Produto save(@RequestBody Produto produto) {
 		return produtoRepository.save(produto);
 	}
+	
+	public void update(@PathVariable Integer id , @RequestBody Produto produto) {
+	      produtoRepository
+		      .findById(id)
+		      .map(p -> {
+		    	  produto.setId(p.getId());
+		    	  produtoRepository.save(produto);
+		    	  return produto;
+		      }).orElseThrow(() -> new 
+		    		  ResponseStatusException(HttpStatus.NOT_FOUND,
+		    				  "Produto não encontrado"));
+	}
+	
+	
 
 }
