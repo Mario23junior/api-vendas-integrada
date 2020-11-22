36b7120d51572a977af17482737bd87543337de4
Mario_junior23 <mariojunior3251@gmail.com>
2020-11-21 23:04:33 -0300

    adicionando getters e setters em PedidoDTO - ItemsPedidoDTO

--- src/main/java/vendas/PedidoDTO/ItemPedidoDTO.java
+++ src/main/java/vendas/PedidoDTO/ItemPedidoDTO.java
@@ -4,4 +4,25 @@ public class ItemPedidoDTO {
      
 	private Integer produto;
 	private Integer quantidade;
+	
+	public ItemPedidoDTO() {
+ 	}
+
+	public Integer getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Integer produto) {
+		this.produto = produto;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	
 }
--- src/main/java/vendas/PedidoDTO/PedidoDTO.java
+++ src/main/java/vendas/PedidoDTO/PedidoDTO.java
@@ -8,4 +8,32 @@ public class PedidoDTO {
    private Integer cliente;
    private BigDecimal total;
    private List<ItemPedidoDTO> items;
+   
+	public PedidoDTO() {
+ 	}
+	
+	public Integer getCliente() {
+		return cliente;
+	}
+	
+	public void setCliente(Integer cliente) {
+		this.cliente = cliente;
+	}
+	
+	public BigDecimal getTotal() {
+		return total;
+	}
+	
+	public void setTotal(BigDecimal total) {
+		this.total = total;
+	}
+	
+	public List<ItemPedidoDTO> getItems() {
+		return items;
+	}
+	
+	public void setItems(List<ItemPedidoDTO> items) {
+		this.items = items;
+	}
+   
 }
