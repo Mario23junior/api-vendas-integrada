68743f2b4515a7ec6cedb3d7ab0b3474faf257a5
Mario_junior23 <mariojunior3251@gmail.com>
2020-11-22 04:04:40 -0300

    Salvando pedidos e itens no banco de dados/ implementando camada DTO

--- src/main/java/vendas/Exception/RegrasNegocioException.java
+++ src/main/java/vendas/Exception/RegrasNegocioException.java
@@ -2,6 +2,8 @@ package vendas.Exception;
 
 public class RegrasNegocioException extends RuntimeException{
    
+	private static final long serialVersionUID = 1L;
+
 	public RegrasNegocioException(String message) {
 		super(message);
 	}
--- src/main/java/vendas/Service/impl/PedidoServicelmpl.java
+++ src/main/java/vendas/Service/impl/PedidoServicelmpl.java
@@ -1,14 +1,22 @@
 package vendas.Service.impl;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
 import vendas.Entity.Cliente;
+import vendas.Entity.ItemPedido;
 import vendas.Entity.Pedido;
+import vendas.Entity.Produto;
 import vendas.Exception.RegrasNegocioException;
+import vendas.PedidoDTO.ItemPedidoDTO;
 import vendas.PedidoDTO.PedidoDTO;
 import vendas.Repository.Clientes;
+import vendas.Repository.ItemsPedido;
 import vendas.Repository.Pedidos;
 import vendas.Repository.Produtos;
 import vendas.Service.PedidoService;
@@ -19,26 +27,94 @@ public class PedidoServicelmpl implements PedidoService {
 	private Pedidos pedidoRepository;
 	private Clientes clientesRepository;
 	private Produtos produtosRepository;
-	
+	private ItemsPedido itemsPedidoRepository;
+ 	
 	public PedidoServicelmpl(Pedidos pedidoRepository, Clientes clientesRepository, 
-			Produtos produtosRepository ) {
+			Produtos produtosRepository , ItemsPedido itemsPedidoRepository ) {
 	       this.pedidoRepository = pedidoRepository;
 	       this.clientesRepository = clientesRepository;
 	       this.produtosRepository = produtosRepository;
+	       this.itemsPedidoRepository = itemsPedidoRepository;
 	}
-
+	
+    @Override
+	@Transactional
  	public Pedido salvar(PedidoDTO dto) {
  		Integer idcliente = dto.getCliente();
- 		clientesRepository
+ 		Cliente cliente = clientesRepository
 	 		.findById(idcliente)
 	 		.orElseThrow(() -> new RegrasNegocioException("Codigo de cliente Invalido"));
  		
  		Pedido pedido = new Pedido();
  		pedido.setTotal(dto.getTotal());
  		pedido.setDataPedido(LocalDate.now());
- 		pedido.setcliente();
+ 		pedido.setCliente(cliente);
  		
-  		return null;
+ 		List<ItemPedido> itemsPedido =  converterItems(pedido, dto.getItems());
+ 		pedidoRepository.save(pedido);
+ 		itemsPedidoRepository.saveAll(itemsPedido);
+ 		pedido.setItens(itemsPedido);
+ 		
+  		return pedido;
 	}
+ 	
+ 	
+ 	
+ 	
+
+ 	private List<ItemPedido> converterItems(Pedido pedido, List<ItemPedidoDTO> items) {
+ 		if(items.isEmpty()) {
+ 			throw new RegrasNegocioException("Não e possivel realizar um pedido sem items");
+ 		}
+ 		
+ 		return items
+ 				.stream()
+ 				.map(dto -> {
+ 					Integer idProduto = dto.getProduto();
+ 					Produto produto = produtosRepository
+ 					.findById(idProduto)
+ 					.orElseThrow(
+ 							() -> new RegrasNegocioException("Codigo de produto invalido : "+ idProduto
+ 						));
+ 					
+ 					ItemPedido itemPedido = new ItemPedido();
+ 					itemPedido.setQuantidade(dto.getQuantidade());
+ 					itemPedido.setPedido(pedido);
+ 					itemPedido.setProduto(produto);
+ 					return itemPedido;
+ 					
+ 				}).collect(Collectors.toList());
+ 	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
