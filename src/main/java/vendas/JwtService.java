44f8efdb57dd0ee7ca52f585d8628f1b166ae690
Mario_junior23 <mariojunior3251@gmail.com>
2020-11-28 07:08:03 -0300

    Decodificando token

--- src/main/java/vendas/JwtService.java
+++ src/main/java/vendas/JwtService.java
@@ -10,6 +10,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import vendas.Entity.Usuario;
@@ -46,14 +48,41 @@ public class JwtService {
 		Instant instant = dataHoraExpiracao.atZone(ZoneId.systemDefault()).toInstant();
 		Date data = Date.from(instant);
 		
+  		
 		return Jwts
 				.builder()
 				.setSubject(usuario.getLogin())
 				.setExpiration(data)
-				.signWith( SignatureAlgorithm.ES512,chaveAssinatura)
+ 				.signWith( SignatureAlgorithm.ES512,chaveAssinatura)
 				.compact();
   	}
 	
+	
+      private Claims ObterClaims(String token) throws ExpiredJwtException {
+    	  return Jwts
+    			  .parser()
+    			  .setSigningKey(chaveAssinatura)
+    			  .parseClaimsJwt(token)
+    			  .getBody();
+      }
+      
+      
+      public boolean Tokenvalido(String token) {
+    	  try {
+    		  Claims claims = ObterClaims(token);
+    		  Date dataExpiracao = claims.getExpiration();
+    		  LocalDateTime data = dataExpiracao.toInstant()
+    				        .atZone(ZoneId.systemDefault()).toLocalDateTime();
+    		  return !LocalDateTime.now().isAfter(data);
+    		  
+    	  }catch (Exception e) {
+			 return false;
+		}
+      }
+      
+      public String obterUsuario(String token) throws ExpiredJwtException {
+    	  return (String) ObterClaims(token).getSubject();
+      }
 	
 	 public static void main(String[] args) {
 		   
@@ -63,6 +92,11 @@ public class JwtService {
 		  Usuario usuario = Usuario.builder().login("fulano").build();
 		  String token = service.gerarToken(usuario);
 		  System.out.println(token);
+		  
+		  boolean isTokenValido = service.Tokenvalido(token);
+		  System.out.println("O teken setá válido "+ isTokenValido);
+		  
+		  System.out.println(service.obterUsuario(token));
  	}
 }
 	
